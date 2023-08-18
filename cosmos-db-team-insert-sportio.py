df6d905da76ee574e85cadcd55198fbeeeffe11e
Szostek, Alex <AlexSzostek@rocketauto.com>
2023-08-18 15:03:01 -0400

    updating team script to upsert changes

--- cosmos-db-team-insert-sportio.py
+++ cosmos-db-team-insert-sportio.py
@@ -1,6 +1,5 @@
 import http.client
 import json
-import uuid
 from azure.cosmos import CosmosClient, exceptions
 
 # Cosmos DB settings
@@ -11,9 +10,7 @@ container_name = "Teams"
 
 # API request to get data
 conn = http.client.HTTPSConnection("api.sportsdata.io")
-payload = ''
-headers = {}
-conn.request("GET", "/v3/cfb/scores/json/TeamsBasic?key=257077aa8bbe453ab5270f8a471be129", payload, headers)
+conn.request("GET", "/v3/cfb/scores/json/TeamsBasic?key=257077aa8bbe453ab5270f8a471be129")
 res = conn.getresponse()
 data = res.read()
 
@@ -28,15 +25,27 @@ client = CosmosClient(endpoint_uri, primary_key)
 database = client.get_database_client(database_name)
 container = database.get_container_client(container_name)
 
-# Insert each record as a new document into Cosmos DB
-for record in teams_data:
-    # Generate a GUID for the "id" field
-    record["id"] = str(uuid.uuid4())  # Generate a new UUID and convert it to string
-    
-    # Insert document into Cosmos DB
+def insert_or_replace_document(container, record_id, record):
     try:
-        response = container.create_item(body=record)
-        print(f"Inserted document with ID: {response['id']}")
+        container.replace_item(item=record_id, body=record)
+        print(f"Replaced document with ID: {record_id}")
+    except exceptions.CosmosResourceNotFoundError:
+        container.create_item(body=record)
+        print(f"Inserted document with ID: {record_id}")
     except exceptions.CosmosHttpResponseError as e:
         print(f"An error occurred: {e}")
 
+# Insert each record as a new document into Cosmos DB
+for record in teams_data:
+    record["id"] = str(record["School"]) # Generate a new UUID and convert it to string
+    record_id = str(record["School"])  # Use the school name as the document ID
+    
+    # SQL query to check if the document exists
+    query = f"SELECT VALUE COUNT(1) FROM c WHERE c.id = '{record_id}'"
+    count_query_result = container.query_items(query=query, enable_cross_partition_query=True)
+    record_count = next(count_query_result)
+    
+    if record_count > 0:
+        insert_or_replace_document(container, record_id, record)
+    else:
+        insert_or_replace_document(container, record_id, record)
